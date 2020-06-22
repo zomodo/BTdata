948f841ad10677181d6ca47dafc4523b5231e0c1
zhangmengdi <986001564@qq.com>
2020-06-22 18:01:53 +0800

    补充前端与后台设计

--- peixuncenter/urls.py
+++ peixuncenter/urls.py
@@ -27,15 +27,19 @@ urlpatterns = [
     path('cate3/', views.cate3, name='cate3'),
     path('cate4/', views.cate4, name='cate4'),
     path('cate5/', views.cate5, name='cate5'),
-    path('cate6/', views.cate6, name='cate6'),
     path('cate7/', views.cate7, name='cate7'),
+
+
     path('cate1/<int:id>/', views.cate_detail, name='cate_detail1'),
     path('cate2/<int:id>/', views.cate_detail, name='cate_detail2'),
     path('cate3/<int:id>/', views.cate_detail, name='cate_detail3'),
     path('cate4/<int:id>/', views.cate_detail, name='cate_detail4'),
     path('cate5/<int:id>/', views.cate_detail, name='cate_detail5'),
-    path('cate6/<int:id>/', views.cate_detail, name='cate_detail6'),
     path('cate7/<int:id>/', views.cate_detail, name='cate_detail7'),
+
+    path('sharefile/', views.sharefile, name='sharefile'),
+    path('sharefile/show/<int:id>/', views.sharefile, name='sharefile_show'),
+    path('sharefile/download/<int:id>/', views.sharefile_download, name='sharefile_download'),
 
 
 ]
